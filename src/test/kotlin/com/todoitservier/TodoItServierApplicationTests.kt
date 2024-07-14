b4426d637330d30ac21eac30cbb669b6c892ddc8
seo <tjalsdud89@naver.com>
2024-07-14 20:35:09 +0900

    init

---
+++ src/test/kotlin/com/todoitservier/TodoItServierApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.todoitservier
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoItServierApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
